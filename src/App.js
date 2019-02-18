dabe75524acbaf0e0dac66fd1782c47366d583e8
unknown <estebanbotero1036@gmail.com>
2019-02-18 18:58:43 -0500

    entrega 1

--- src/App.js
+++ src/App.js
@@ -3,23 +3,54 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      tareas:["Sacar la ropa", "Hacer la cama", "Leer un rato"],
+      valor:""
+    }
+  }
+
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {
+              this.state.tareas.map(
+                (tarea,i)=>
+                <li key={i}>{tarea}</li>
+              )
+            }
+
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onKeyPress={this.handleKeyPress}>
+             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" onChange={this.ObtenerTarea.bind(this)} value={this.state.valor} />
            </form>
         </div>
       </div>
     )
   }
+
+  ObtenerTarea(event) {
+    this.setState({
+      valor: event.target.value
+    })
+  }
+
+  handleKeyPress = (event) => {
+  if(event.key === 'Enter'){
+    event.preventDefault();
+    this.setState({
+      tareas: this.state.tareas.concat(this.state.valor),
+      valor:""
+    })
+  }
+}
 }
 
 
